f79f71ab9855b49d2af83c5b3b0ac77cf6c6abf1
mohamed-h-eng <mohamedhany1615@gmail.com>
2026-02-16 18:05:25 +0200

    Done phase 2

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ async function DBConnection(){
 }
 
 DBConnection()
+var login_session = 0;
 
 app.get("/",(req,res)=>{
     res.send("welcome to the root")
@@ -63,8 +64,96 @@ app.post("/api/login",async(req,res)=>{
         res.status(200).json({
             msg:"login success"
         })
+        login_session = 123456
     }catch(error){
         console.log("error login")
+    }
+})
+
+const Product = require("./models/Product")
+
+app.post("/api/products",async(req,res)=>{
+    try{
+        // get data
+        const {name, price, stock} = req.body
+        const {id} = req.query
+        // validate
+        const [user] = await User.find({_id:id})
+        if(user.role == "admin"){
+            Product.create({name,price,stock});
+        }else{
+            return res.status(404).json({msg:"unathourized access"})
+        }
+        const product = await Product.findOne({name})
+        if(product) return res.status(201).json({msg:"Product already exist"})
+        // response
+        res.status(200).json({
+            msg:"Product added successfully"
+        })
+    }catch(error){
+        console.log("error creating product")
+        console.log(error)
+    }
+})
+
+app.get("/api/products",async(req,res)=>{
+    try{
+        // get data
+        const {name} = req.body
+        // validate
+        const product = await Product.findOne({name})
+        if(!product) return res.status(404).json({msg:"Product not found"})
+        // response
+        res.status(200).json({
+            msg:"Product found",
+            data:product
+        })
+    }catch(error){
+        console.log("error creating product")
+    }
+})
+
+app.get("/api/search",async(req,res)=>{
+    try{
+        // get data
+        const {name} = req.query
+        // validate
+        const product = await Product.findOne({name})
+        if(!product) return res.status(404).json({msg:"Product not found"})
+        // response
+        res.status(200).json({
+            msg:"Product found",
+            data:product
+        })
+    }catch(error){
+        console.log("error creating product")
+    }
+})
+
+const Cart = require("./models/Cart")
+app.post("/api/cart",async(req,res)=>{
+    try{
+        // get data
+        const {user_id, items, total, number_of_items} = req.body
+        // validate
+        let cart = await Cart.findOne({user_id})
+        if(!cart){
+            cart= await Cart.create({user_id, items,total,number_of_items});
+        }else{
+            return res.status(201).json({
+                msg:"cart already exist just udpate items",
+                data:await cart.populate("items")
+                
+            })
+        }
+        // response
+        res.status(200).json({
+            msg:"Cart created successfully",
+            data:await cart.populate("items")
+        })
+    }catch(error){
+        console.log("error creating cart")
+        console.log(error)
     }
 })
 app.listen(port,()=>{
---
+++ models/Cart.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const cartSchema = new mongoose.Schema({
+    user_id:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    items:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product"
+    }],
+    total:Number,
+    number_of_items:Number
+
+},{timestamps:true})
+
+
+const Cart = mongoose.model("Cart",cartSchema)
+module.exports = Cart
